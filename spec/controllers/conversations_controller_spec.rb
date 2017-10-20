797bce81942aa3ec5fee0d641dd70dde774f1d47
Eric Loos <ericloos00@gmail.com>
2017-10-19 20:41:44 -0500

    created index tests for company and customer

--- spec/controllers/conversations_controller_spec.rb
+++ spec/controllers/conversations_controller_spec.rb
@@ -2,6 +2,35 @@ require 'rails_helper'
 
 RSpec.describe ConversationsController, type: :controller do
 
+
+  describe 'GET #index' do
+    context 'company signed in' do
+      it 'assigns all conversations for that company to @conversations' do
+          company = create :company
+          customer = create :customer
+          customer_request_1 = create :customer_request, customer_id: customer.id
+          quote_1 = create :quote, customer_request_id: customer_request_1.id, company_id: company.id
+          conversation_1 = create :conversation, quote_id: quote_1.id, company_id: company.id, customer_id: customer.id
+          sign_in company
+          get :index
+          expect(assigns(:conversations)).to match_array [conversation_1]
+      end
+    end
+    context 'customer signed in' do
+      it 'assigns all conversations for that customer to @conversations' do
+          company = create :company
+          customer = create :customer
+          customer_request_1 = create :customer_request, customer_id: customer.id
+          quote_1 = create :quote, customer_request_id: customer_request_1.id, company_id: company.id
+          conversation_1 = create :conversation, quote_id: quote_1.id, company_id: company.id, customer_id: customer.id
+          sign_in customer
+          get :index
+          expect(assigns(:conversations)).to match_array [conversation_1]
+      end
+    end
+
+  end
+
   describe '#create' do
     context 'no one is signed in' do
       it 'should not create a conversation' do
